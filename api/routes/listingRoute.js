3a59b746bf8f444b3a217219e10f24735fb0f589
Harsh Raj <harshr784@gmail.com>
2024-02-24 17:48:26 +0530

    create listing

---
+++ api/routes/listingRoute.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import { createListing } from '../controllers/listingController.js';
+import { verifyToken } from '../utils/verifyUser.js';
+const router=express.Router();
+
+router.post('/create',verifyToken, createListing)
+export default router;
