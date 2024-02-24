7e0efbc3ae4eac23f27a73ceb6754d7de7a39773
Harsh Raj <harshr784@gmail.com>
2024-02-24 11:02:48 +0530

    update user

--- api/index.js
+++ api/index.js
@@ -3,9 +3,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from './routes/userRoute.js'
 import authRouter from './routes/authRoute.js'
+import cookieParser from 'cookie-parser';
 dotenv.config();
+
 const app =express();
 app.use(express.json());
+app.use(cookieParser());
+
+
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected to mongoDB")
 })
