f857537d9b207a0adc7522d86e26922236e305ad
Harsh Raj <harshr784@gmail.com>
2024-02-29 18:49:54 +0530

    search bugs

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -3,7 +3,7 @@ import { MdLocationOn } from 'react-icons/md';
 
 export default function ListingItem({ listing }) {
   return (
-    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
+    <div className='bg-white shadow-md transition duration-300 ease-in-out transform hover:shadow-lg hover:scale-105 overflow-hidden rounded-lg w-full sm:w-[330px]'>
       <Link to={`/listing/${listing._id}`}>
         <img
           src={
@@ -11,38 +11,27 @@ export default function ListingItem({ listing }) {
             'https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg'
           }
           alt='listing cover'
-          className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
+          className='h-[320px] sm:h-[220px] w-full object-cover transition duration-300 ease-in-out transform hover:scale-105'
         />
         <div className='p-3 flex flex-col gap-2 w-full'>
-          <p className='truncate text-lg font-semibold text-slate-700'>
-            {listing.name}
-          </p>
+          <p className='truncate text-lg font-semibold text-slate-700 hover:text-slate-500 transition duration-300'>{listing.name}</p>
           <div className='flex items-center gap-1'>
-            <MdLocationOn className='h-4 w-4 text-green-700' />
-            <p className='text-sm text-gray-600 truncate w-full'>
-              {listing.address}
-            </p>
+            <MdLocationOn className='h-6 w-6 text-green-700' />
+            <p className='text-sm text-gray-600 truncate w-full'>{listing.address}</p>
           </div>
-          <p className='text-sm text-gray-600 line-clamp-2'>
-            {listing.description}
-          </p>
-          <p className='text-slate-500 mt-2 font-semibold '>
-            $
-            {listing.offer
+          <p className='text-sm text-gray-600 line-clamp-2'>{listing.description}</p>
+          <p className='text-red-500 mt-2 font-semibold'>
+          <b>₹</b>{listing.offer
               ? listing.discountPrice.toLocaleString('en-US')
               : listing.regularPrice.toLocaleString('en-US')}
             {listing.type === 'rent' && ' / month'}
           </p>
           <div className='text-slate-700 flex gap-4'>
             <div className='font-bold text-xs'>
-              {listing.bedrooms > 1
-                ? `${listing.bedrooms} beds `
-                : `${listing.bedrooms} bed `}
+              {listing.bedrooms > 1 ? `${listing.bedrooms} beds ` : `${listing.bedrooms} bedroom `}
             </div>
             <div className='font-bold text-xs'>
-              {listing.bathrooms > 1
-                ? `${listing.bathrooms} baths `
-                : `${listing.bathrooms} bath `}
+              {listing.bathrooms > 1 ? `${listing.bathrooms} baths ` : `${listing.bathrooms} bathroom `}
             </div>
           </div>
         </div>
