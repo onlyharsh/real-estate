97d674b31a9ebdc2bd3646b9062a31ae00c297e4
Harsh Raj <harshr784@gmail.com>
2024-03-04 21:45:09 +0530

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'https://real-estate-1.onrender.com',
+        target:'https://trendyhomes.onrender.com',
         secure: false,
       }
     }
