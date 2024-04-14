dd802de3dee6344a7bed24829acaa784484217f6
Harsh Raj <harshr784@gmail.com>
2024-04-15 01:02:01 +0530

    gg

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -303,7 +303,7 @@ export default function Profile() {
         <div className='fixed inset-0 z-50 flex items-center p-3 justify-center bg-gray-900 bg-opacity-50'>
           <div className='bg-white p-6 rounded-lg'>
             <h2 className='text-xl font-semibold mb-4'>Confirm Deletion</h2>
-            <p className='text-gray-600 mb-4'>Are you sure you want to delete this listing? This action cannot be undone.</p>
+            <p className='text-gray-600 mb-4'>Are you sure you want to delete your account? This action cannot be undone.</p>
             <div className='flex justify-end'>
               <button onClick={closeDeleteConfirmation} className='bg-gray-300 text-gray-700 px-4 py-2 rounded mr-4 hover:bg-gray-400'>
                 Cancel
